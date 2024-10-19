df231cb1add37f910d55b9e5d19021362bc3190b
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-10-19 22:39:02 +0200

    make main menu syle

---
+++ src/MonoBehaviours/PaperText.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using UnityEngine;
+
+namespace StoriesOfaHkPlayer_Ch2.MonoBehaviours;
+
+public class PaperText : MonoBehaviour
+{
+    public bool DisplayText = false;
+    public UnityEngine.UI.Text TextComponent;
+}
